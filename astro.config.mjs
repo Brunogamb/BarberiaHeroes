3ff7c6511e695a93950735db200800562f48620c
Brunogamb <brunogamberini24@gmail.com>
2025-11-06 19:58:14 -0300

    bug fix header links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://brunogamb.github.io/BarberiaHeroes/',
-  base: '/BarberiaHeroes/', // nombre exacto del repositorio
+  site: 'https://brunogamb.github.io/BarberiaHeroes/'
 });
