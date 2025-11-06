43218c805e2926d48a920ed788d5cfddf4ed023a
Brunogamb <brunogamberini24@gmail.com>
2025-11-06 20:03:04 -0300

    add base path configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://brunogamb.github.io/BarberiaHeroes/'
+  site: 'https://brunogamb.github.io/BarberiaHeroes/',
+  base: '/BarberiaHeroes/'
 });
