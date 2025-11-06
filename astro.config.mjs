1b4eb1b9d6cfe76d01ad8a8dcd9c10fd3e9be067
Brunogamb <brunogamberini24@gmail.com>
2025-11-06 19:54:15 -0300

    Configurar base para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://brunogamb.github.io/BarberiaHeroes/',
+  base: '/BarberiaHeroes/', // nombre exacto del repositorio
+});
